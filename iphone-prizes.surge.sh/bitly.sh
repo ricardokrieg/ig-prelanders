e7c557e1e04af5fad2977886c6c4feb5016ff4cc
Ricardo Franco <ricardo.krieg@gmail.com>
2021-06-12 10:03:09 -0300

    iphone prizes

---
+++ iphone-prizes.surge.sh/bitly.sh
@@ -0,0 +1,2 @@
+#!/bin/zsh
+curl -X POST -H "Authorization: Bearer aa508b2a58b9a0ffa6bb07e9f39446f4f48f7cf5" -H 'Content-Type: application/json' https://api-ssl.bitly.com/v4/shorten -d '{"domain": "bit.ly", "long_url": "http://iphone-prizes.surge.sh/#ulfdugrnulhj:::Ulfdugr+Iudqfr:::/image.jpg"}'
